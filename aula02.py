292bbc636e54b5ce1677c4b88fbd1f17ddcce61e
luis henrique <luis.ferreira.silva19@escola.pr.gov.br>
2026-02-18 12:43:58 -0300

    primeira aula de programação

---
+++ aula02.py
@@ -0,0 +1,10 @@
+numero = 500 # variavel int 
+numero_quebrado = 3.56 # variavel float 
+a_bola_esta_furada = False # variavel booleana 
+texto = "primeira aula pratica" # variavel de texto 
+
+
+print(numero)
+print(numero_quebrado)
+print(a_bola_esta_furada)
+print(texto)
