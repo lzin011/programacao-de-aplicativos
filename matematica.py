fdb4ed17ad93fa365b035c83a01a169235e0776f
luis henrique <luis.ferreira.silva19@escola.pr.gov.br>
2026-02-20 09:51:40 -0300

    aula 20/02

---
+++ matematica.py
@@ -0,0 +1,17 @@
+valor1 = int(input("digite o valor1"))
+valor2 = int(input("digite o valor2"))
+
+soma = valor1 + valor2 
+subtracao = valor1 - valor2 
+divisao = valor1 / valor2 
+multiplicacao = valor1 * valor2 
+
+
+print("A soma é", soma) 
+print("A subtracao é", subtracao)
+print("a divisao é", divisao)
+print("a multiplicacao é", multiplicacao)
+
+
+
+
