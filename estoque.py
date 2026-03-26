ae38b3becad296abd2885a3130b3abcab5e8f901
luis <luis.ferreira.silva19@escola.pr.gov.br>
2026-03-26 10:11:50 -0300

    auladada

---
+++ estoque.py
@@ -0,0 +1,6 @@
+numeros = [100,250,333,446,5689]
+soma = 0
+
+for numero in numeros:
+    soma+=numero
+print(soma)
---
+++ login.py
@@ -0,0 +1,8 @@
+entrada = ""
+
+while entrada != "xuxu": 
+    entrada = input("Digite a senha : ")
+    print("Você digitou :", entrada)
+
+print("acesso liberado.")                                                
+
