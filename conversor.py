ddb14d24ecc206f2a9f3f0c823f87a32b4c5e511
luis <luis.ferreira.silva19@escola.pr.gov.br>
2026-04-08 11:32:33 -0300

    auladada

---
+++ conversor.py
@@ -0,0 +1,3 @@
+valor = float(input("Digite o valor em U$ que você deseja converter: "))
+valor = (valor / 5)
+print(f"O valor convertido em  é {valor}")
---
+++ posiouneg.py
@@ -0,0 +1,7 @@
+numero = float(input("Digite um número: "))
+if numero > 0:
+    print(f"O número {numero} é positivo")
+elif numero < 0:
+    print(f"O número {numero} é negativo")
+else:
+    print(f"O número {numero} é neutro")
