750d01bacd9c52605d08e7d412ecf2b7081ec56e
luis <luis.ferreira.silva19@escola.pr.gov.br>
2026-05-14 10:03:36 -0300

    auladada

--- desafio.py
+++ desafio.py
@@ -3,7 +3,20 @@ def criar_arquivo():
 
 def criar():
     lugar = input("destino: ")
-    with open('alunos.txt','a') as f:
+    with open('destino.txt','a') as f:
         f.write(lugar + '\n')
     print("destino adicionado à lista!")
 criar()
+
+
+def lista():
+    with open('alunos.txt','r') as f:
+        alunos = f.readlines()
+
+
+def atualizar():
+    ler()
+    idx = int(input("digite o ID do destino que quer alterar: "))
+    novo_nome = input ("novo destino: ")
+    with open ('viagens.txt', 'r') as f:
+        lugar = f.readlines()0
