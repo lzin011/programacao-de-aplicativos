449dd875d181b92f708c3b115bbe73cd7bc78d80
luis <luis.ferreira.silva19@escola.pr.gov.br>
2026-05-20 12:41:43 -0300

    auladada

---
+++ cadastro.py
@@ -0,0 +1,13 @@
+import json
+
+cpf_aluno = int(input("digite seu cpf: ")
+nome_aluno = input("qual o seu nome: ")
+telefone_aluno = int(input("qual o seu telefone ")
+turma = input("qual a sua turma: ")
+idade_aluno = int(input("qual sua idade: "))
+
+aluno = {"cpf":cpf_aluno
+         "nome":nome_aluno
+         "telefone":telefone_aluno
+         "turma":turma
+         "idade":idade_aluno }
