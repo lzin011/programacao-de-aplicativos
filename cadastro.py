83431df16e532a7daa3bac4fc047ba39468506df
luis <luis.ferreira.silva19@escola.pr.gov.br>
2026-05-21 10:07:33 -0300

    auladada

--- cadastro.py
+++ cadastro.py
@@ -1,13 +1,19 @@
 import json
 
-cpf_aluno = int(input("digite seu cpf: ")
-nome_aluno = input("qual o seu nome: ")
-telefone_aluno = int(input("qual o seu telefone ")
-turma = input("qual a sua turma: ")
-idade_aluno = int(input("qual sua idade: "))
+def cadastrar_aluno():
+    cpf_aluno = int(input("digite seu cpf: "))
+    nome_aluno = input("qual o seu nome: ")
+    telefone_aluno = int(input("qual o seu telefone "))
+    turma = input("qual a sua turma: ")
+    idade_aluno = int(input("qual sua idade: "))
 
 aluno = {"cpf":cpf_aluno
          "nome":nome_aluno
          "telefone":telefone_aluno
          "turma":turma
          "idade":idade_aluno }
+with open (f"cadastro.json",'r')as arquivo:
+    json.dump(aluno,cadastro.json, indent=4, ensure-ascii=false)
+
+
+
