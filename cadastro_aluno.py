3aca49dd7947f1cc444e6e46e85ab56543c51500
luis <luis.ferreira.silva19@escola.pr.gov.br>
2026-06-11 10:05:57 -0300

    auladada

--- cadastro_aluno.py
+++ cadastro_aluno.py
@@ -1,6 +1,6 @@
 import sqlite3
-conexão = sqlite3.connect ('escola.db')
-sursor = conexão.cursor()
+conexao = sqlite3.connect ('escola.db')
+cursor = conexao.cursor()
 cursor.execute ('''CREATE TABLE IF NOT EXISTS alunos (
                     id INTEGER PRIMARY KEY AUTOINCREMENT,
                     nome TEXT NOT NULL,
@@ -18,9 +18,9 @@ idade_aluno = int(input("IDADE: "))
 CPF_aluno = input("CPF: ")
 
 comando_inserir = (f'''
-                    INSERT into alunos (nome, telefone, idade,cpf)
+                    INSERT into alunos (nome, telefone, turma, idade,cpf)
                     VALUES('{nome_aluno}','{telefone_aluno}','{turma_aluno}',{idade_aluno},'{CPF_aluno}')''')
                         
 cursor.execute(comando_inserir)
-conexão.commit()
-conexão.close 
+conexao.commit()
+conexao.close 
