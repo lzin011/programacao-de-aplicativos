37ee17babb72b6c358809a36ce244890c142fdaa
luis <luis.ferreira.silva10@escola.pr.gov.br>
2026-08-20 10:15:12 -0300

    auladada

--- assert.py
+++ assert.py
@@ -1,6 +1,5 @@
 def eh_par(numero):
-    try:
-        return numero % 2 == 0
+    return numero % 2 == 0
 
 assert eh_par(4) is True          
 assert eh_par(5) is False        
@@ -27,14 +26,22 @@ assert situacao_aluno(5.9) == "Recuperação"
 #----------------------------------------
 
 def calcular_desconto(preco, percentual):
+
+    preco = float(preco)
+    percentual = float(percentual)
+
     return preco - (preco * percentual / 100)
 
 
 assert calcular_desconto(100, 0) == 100      
-assert calcular_desconto(100, 10) == 90     
-assert calcular_desconto(100, 50) == 50       
-assert calcular_desconto(100, 100) == 0       
-assert calcular_desconto(99.90, 10) == 89.91  
+assert calcular_desconto(100, 10.0) == 90     
+assert calcular_desconto(100, 50.0) == 50       
+assert calcular_desconto(100, 100.0) == 0       
+assert calcular_desconto(99.90, 10.0) == 89.91
+
+def verificar_acesso(idade, acompanhado):
+    if idade >= 18 or acompanhado 
+
 
 
 
